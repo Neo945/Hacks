c3656427255609f95821ee31d0174ef013f653f2
Neo945 <shreeshsrvstv@gmail.com>
2022-03-09 22:54:49 +0530

    Add: skills controller

--- server/Express/src/models/skills.js
+++ server/Express/src/models/skills.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-await-in-loop */
 const mongoose = require('mongoose');
 
 const SkillsSchema = new mongoose.Schema({
@@ -7,11 +8,23 @@ const SkillsSchema = new mongoose.Schema({
         trim: true,
         minlength: [3, 'Skill length less than 3'],
     },
-    user: {
-        type: mongoose.Types.ObjectId,
-        ref: 'BaseUser',
-    },
 });
+
+SkillsSchema.statics.getSkills = async function (skills, user) {
+    for (let i = 0; i < skills.length; i += 1) {
+        const skill = await this.findOne({
+            skill: skills[i],
+        });
+        if (!skill) {
+            const newSkill = new this({
+                skill,
+            });
+            await newSkill.save();
+            user.skills.push(newSkill);
+            await user.save();
+        }
+    }
+};
 
 const Skill = mongoose.model('Skill', SkillsSchema);
 module.exports = Skill;
